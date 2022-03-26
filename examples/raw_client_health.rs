fe89d25d8ff8be8c1dcb3aa59530b1d5b7b38dde
Bradley Smith <bradley@baysmith.com>
2022-03-26 00:42:37 -0700

    Add a digital version of a health meter

--- examples/raw_client_health.rs
+++ examples/raw_client_health.rs
@@ -8,6 +8,7 @@ use serde_json::json;
 async fn main() -> Result<()> {
     let mut client = GameSenseClient::new("RAW_CLIENT_HEALTH", "Example Health Meter", "baysmith", None).await?;
 
+    // Handler for percentage meter
     client.raw_client.send_data("bind_game_event", &json!({
         "game": "RAW_CLIENT_HEALTH",
         "event": "HEALTH",
@@ -32,8 +33,58 @@ async fn main() -> Result<()> {
         }],
     })).await?;
 
+    // Handlers for a digital F1-F10 meter
+    let on_handler = |key| {
+        json!({
+            "device-type": "rgb-per-key-zones",
+            "zone": key,
+            "color": {
+                "red": 255,
+                "green": 0,
+                "blue": 0,
+            },
+            "mode": "color",
+        })
+    };
+    let off_handler = |key| {
+        json!({
+            "device-type": "rgb-per-key-zones",
+            "zone": key,
+            "color": {
+                "red": 0,
+                "green": 0,
+                "blue": 0,
+            },
+            "mode": "color",
+        })
+    };
+
+    let meter_event = |value| {
+        let mut handlers = Vec::new();
+        for f in 1..11 {
+            if value >= f {
+                handlers.push(on_handler(format!("f{}", f)));
+            } else {
+                handlers.push(off_handler(format!("f{}", f)));
+            }
+        }
+        json!({
+            "game": "RAW_CLIENT_HEALTH",
+            "event": format!("HEALTH{}", value),
+            "handlers": handlers,
+        })
+    };
+
+    for health in 0..11 {
+        client.raw_client.send_data("bind_game_event", &meter_event(health)).await?;
+    }
+
+
     client.start_heartbeat();
+
+    // Cycle health meter
     for health in (0..101).step_by(10).rev() {
+        dbg!(health);
         client.raw_client.send_data("game_event", &json!({
             "game": "RAW_CLIENT_HEALTH",
             "event": "HEALTH",
@@ -42,8 +93,21 @@ async fn main() -> Result<()> {
             }
         })).await?;
         tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-        dbg!(health);
     }
+
+    // Cycle digital meter
+    for value in (0..11).rev() {
+        dbg!(value);
+        client.raw_client.send_data("game_event", &json!({
+            "game": "RAW_CLIENT_HEALTH",
+            "event": format!("HEALTH{}", value),
+            "data": {
+                "value": 100,
+            }
+        })).await?;
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    }
+
     client.stop_heartbeat()?;
 
     client.raw_client.send_data("remove_game_event", &json!({
